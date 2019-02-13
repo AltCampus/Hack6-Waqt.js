5bc1375c6b9b19ea7b2c2e57b154c99e49477273
abidRahim <abid.rahim1@gmail.com>
2019-02-13 17:03:19 +0530

    methods

--- waqt.js
+++ waqt.js
@@ -6,3 +6,25 @@ Date.prototype.now = function(){
   return this;
 }
 
+
+// Day of Week Gets the day of the week.
+function getDay(date) {
+	return date.getDay();
+}
+
+// getWeek Gets the week of the Month.
+function getWeek(date) {
+	const day = date.getDate();	
+	return Math.ceil(day / 7);
+}
+
+// returns true, if dates precedes one another
+function isAfter(before, after) {
+	return before < after;
+}
+
+// returns false , if before date does not precede the other
+function isBefore(before, after) {
+  return before > after;
+}
+
