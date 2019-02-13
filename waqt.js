02d57452c355c1c181c35f82183c33b7465d23f5
Abhishek Shastri <shastri48@gmail.com>
2019-02-13 16:48:41 +0530

    add getdaysinyear function

--- waqt.js
+++ waqt.js
@@ -2,7 +2,23 @@ function waqt() {
   return new Date;
 }
 
-Date.prototype.now = function(){
-  return this;
+waqt.now = function(){
+  return new Date;
+}
+
+waqt.getDaysInMonth = function(year, month){
+  let date = new Date(year, month)
+  return Number(date.toJSON().split("T")[0].split("-").slice(-1)[0])
+}
+
+waqt.getDayOfYear = function(date = new Date()){
+  let year = Number(date.toJSON().split("-")[0]);
+  let day = date.getDate();
+  let month = date.getMonth();
+  let totalDays = 0;
+  for(i=1; i<=month; i++){
+    totalDays += Number(new Date(year,i).toJSON().split("T")[0].slice(-2));
+  }
+  return totalDays+day;
 }
 
