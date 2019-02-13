e82980a711e65b8ce2a1f88d2644633caf23dd08
Abhishek Shastri <shastri48@gmail.com>
2019-02-14 00:18:44 +0530

    Update index.js

--- format/index.js
+++ format/index.js
@@ -1,6 +1,6 @@
 // Gives date in format provided
 const format = function (date = new Date(), format) {
-    seperator = format.match(/\W*/g).filter(v => v != "")[0];
+     let seperator = format.match(/\W*/g).filter(v => v != "")[0];
     return format.match(/\w*/g).filter(v => v != "")
     .map(v => (v == 'DD' || v == 'dd') ? date.getDate() : (v == 'MM' || v == 'mm')? date.getMonth() : (v == 'YYYY' || v == 'yyyy') ? date.getFullYear() : "provide correct format"
     ).join(`${seperator}`)
