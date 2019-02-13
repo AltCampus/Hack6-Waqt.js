c1ed80b4084f746a2a9feb9d76652c3f09ec9a51
klassynihal <harshaan.n.khan@gmail.com>
2019-02-13 16:17:29 +0530

    min,max

--- waqt.js
+++ waqt.js
@@ -6,3 +6,18 @@ Date.prototype.now = function(){
   return this;
 }
 
+
+function min(dateArr) {
+  return dateArr.reduce((acc,v) => {
+    if(acc < v) acc = v;
+    return v;
+  });
+}
+
+function max(dateArr) {
+  return dateArr.reduce((acc, v) => {
+    if (acc > v) acc = v;
+    return v;
+  });
+}
+
