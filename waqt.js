e1b1770865201a64473297f8b342e6f6cca839b8
klassynihal <harshaan.n.khan@gmail.com>
2019-02-13 19:01:43 +0530

    solved merge

--- waqt.js
+++ waqt.js
@@ -3,37 +3,40 @@ function waqt() {
 }
 
 // will return current date and time
-waqt.now = function(){
+waqt.now = function () {
   return new Date;
 }
 
+
 // Gives number of days in month according to year and month provided
-waqt.getDaysInMonth = function(year=2018, month=1){
+waqt.getDaysInMonth = function (year = 2018, month = 1) {
   let date = new Date(year, month)
   return Number(date.toJSON().split("T")[0].split("-").slice(-1)[0])
 }
 
 
 // Gives total number of days passed in year
-waqt.getDayOfYear = function(date = new Date()){
+waqt.getDayOfYear = function (date = new Date()) {
   let year = date.getFullYear();
   let day = date.getDate();
   let month = date.getMonth();
   let totalDays = 0;
-  for(i=1; i<=month; i++){
-    totalDays += Number(new Date(year,i).toJSON().split("T")[0].slice(-2));
+  for (i = 1; i <= month; i++) {
+    totalDays += Number(new Date(year, i).toJSON().split("T")[0].slice(-2));
   }
-  return totalDays+day;
+  return totalDays + day;
+}
+
 // It will give you the current datetime
-waqt.prototype.now = function(){
+waqt.prototype.now = function () {
   return this;
 }
 
 
 // Minimum of the given dates Returns the minimum (most distant future) of the given date.
 function min(dateArr) {
-  return dateArr.reduce((acc,v) => {
-    if(acc > v) acc = v;
+  return dateArr.reduce((acc, v) => {
+    if (acc > v) acc = v;
     return acc;
   }).toJSON();
 }
@@ -47,66 +50,55 @@ function max(dateArr) {
 }
 
 // Add the specified number of days to the given date. 
-waqt.prototype.addDays = function(...arr){
-  let newWaqt = arr[0].getTime() + arr[1]*86400*1000; 
+waqt.prototype.addDays = function (...arr) {
+  let newWaqt = arr[0].getTime() + arr[1] * 86400 * 1000;
   return new Date(newWaqt);
 }
+
 
 // Subtract the specified number of days from the given date.
-waqt.prototype.subDays = function(...arr){
-  let newWaqt = arr[0].getTime() - arr[1]*86400*1000; 
+waqt.prototype.subDays = function (...arr) {
+  let newWaqt = arr[0].getTime() - arr[1] * 86400 * 1000;
   return new Date(newWaqt);
 }
 
-
-
 // Gives date in format provided
-function format(date=new Date(), format){
-  seperator = format.match(/\W*/g).filter(v=>v!="")[0];
-  return format.match(/\w*/g).filter(v=>v!="").map(v=>{
-    if(v=='DD'||v=='dd'){
-      return date.getDate();
-    } else if(v=='MM'||v=='mm'){
-      return date.getMonth();
-    } else if(v=='YYYY'||v=='yyyy'){
-      return date.getFullYear();
-    }
-  }).join(`${seperator}`)
+function format(date = new Date(), format) {
+  seperator = format.match(/\W*/g).filter(v => v != "")[0];
+  return format.match(/\w*/g).filter(v => v != "").map(v => (v == 'DD' || v == 'dd') ? date.getDate() : (v == 'MM' || v == 'mm')? date.getMonth() : (v == 'YYYY' || v == 'yyyy') ? date.getFullYear() : "provide correct format"
+  ).join(`${seperator}`)
 }
 
 //Is Same Check if a date is the same as another date.
-function isSameDay(date1,date2) {
+function isSameDay(date1, date2) {
   return (
-    (date1.getDate() == date2.getDate() ) &&
-    (date1.getMonth() == date2.getMonth() ) &&
-    (date1.getFullYear() == date2.getFullYear() )	
+    (date1.getDate() == date2.getDate()) &&
+    (date1.getMonth() == date2.getMonth()) &&
+    (date1.getFullYear() == date2.getFullYear())
   );
-  }
 }
 
 // Return the end of a unit of time for the given date.
-waqt.prototype.endOfDay = function(...arr){
-  let newWaqt = new Date(arr[0].getFullYear(),arr[0].getMonth(),arr[0].getDate(),23,59,59,999) 
+waqt.prototype.endOfDay = function (...arr) {
+  let newWaqt = new Date(arr[0].getFullYear(), arr[0].getMonth(), arr[0].getDate(), 23, 59, 59, 999)
   return new Date(newWaqt);
 }
 
-<<<<<<< HEAD
 // Get the unit of time between the given dates in milliseconds.
-waqt.prototype.differenceInMilliseconds = function(...arr) {
+waqt.prototype.differenceInMilliseconds = function (...arr) {
   return new Date(arr[0]).getTime() - new Date(arr[1].getTime());
 }
 
 // Get the unit of time between the given dates in days.
-waqt.prototype.differenceInDays = function(...arr) {
+waqt.prototype.differenceInDays = function (...arr) {
 
-let timeDifference = Date(arr[0]).getTime() - new Date(arr[1].getTime());
+  let timeDifference = Date(arr[0]).getTime() - new Date(arr[1].getTime());
 
-  return Math.floor(timeDifference/(86400*1000));
+  return Math.floor(timeDifference / (86400 * 1000));
 }
-=======
+
 // Is a Date Check if a variable is a native js Date object.
 function isDate(date) {
   return (date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date));
 }
-  
->>>>>>> fc80cc93f4bcda3b66740a733d2925fb7b0f1d73
+
