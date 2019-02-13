e701392d6bf02df777329dd0a803a6ca2699a35d
logicallycorrect <1994.ravi@gmail.com>
2019-02-13 16:14:34 +0530

    add subDays and addDays functions

--- waqt.js
+++ waqt.js
@@ -2,7 +2,19 @@ function waqt() {
   return new Date;
 }
 
+// It will give you the current datetime
 Date.prototype.now = function(){
   return this;
 }
 
+// Add the specified number of days to the given date. 
+Date.prototype.addDays = function(...arr){
+  let newWaqt = arr[0].getTime() + arr[1]*86400*1000; 
+  return new Date(newWaqt);
+}
+
+// Subtract the specified number of days from the given date.
+Date.prototype.subDays = function(...arr){
+  let newWaqt = arr[0].getTime() - arr[1]*86400*1000; 
+  return new Date(newWaqt);
+}
