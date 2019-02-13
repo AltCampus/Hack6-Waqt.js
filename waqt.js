c8ff428f3854bc48d04eddb0e212c0a8b04a4d6c
logicallycorrect <1994.ravi@gmail.com>
2019-02-13 18:49:47 +0530

    add differenceInMilliseconds and differenceInDays

--- waqt.js
+++ waqt.js
@@ -39,3 +39,16 @@ waqt.prototype.endOfDay = function(...arr){
   let newWaqt = new Date(arr[0].getFullYear(),arr[0].getMonth(),arr[0].getDate(),23,59,59,999) 
   return new Date(newWaqt);
 }
+
+// Get the unit of time between the given dates in milliseconds.
+waqt.prototype.differenceInMilliseconds = function(...arr) {
+  return new Date(arr[0]).getTime() - new Date(arr[1].getTime());
+}
+
+// Get the unit of time between the given dates in days.
+waqt.prototype.differenceInMilliseconds = function(...arr) {
+
+let timeDifference = Date(arr[0]).getTime() - new Date(arr[1].getTime());
+
+  return Math.floor(timeDifference/(86400*1000));
+}
